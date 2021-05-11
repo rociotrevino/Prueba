e1ee393a27b9d0205051841b3f7a2f918da563c5
rociotrevino <80795362+rociotrevino@users.noreply.github.com>
2021-05-11 17:49:46 -0500

    Ejercicio tarea 3
    Programa que pregunta al usuario el número de horas trabajadas y el costo por hora. Después muestra por pantalla la paga correspondiente.

---
+++ Tarea 1-3 Pago total.py
@@ -0,0 +1,6 @@
+#Programa que captura horas trabajadas y costo por hora y muestra la paga correspondiente
+print ("CÁLCULO DE PAGO DEL TRABAJADOR")
+horas=float(input("Horas Trabajadas   "))
+costo=float(input("Costo por Hora     "))
+pago=horas*costo
+print ("El pago es $",pago)
